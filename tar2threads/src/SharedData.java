e8e477b9dd72439f276b4953daa9b421f7507ac4
enabf <enabf@LAPTOP-DR69B9G2>
2024-11-21 12:58:33 +0200

    new

--- tar2threads/src/SharedData.java
+++ tar2threads/src/SharedData.java
@@ -1,12 +1,24 @@
 import java.util.ArrayList;
-
+/**
+ * @author enabfouad
+ * The SharedData class is used to store shared data that is accessed by multiple threads.
+ * It contains the array of integers, the target sum, and a flag to indicate whether 
+ * a solution has been found. It also tracks the elements selected in the subset 
+ * that sums up to the target value.
+ */
 public class SharedData 
 {
 	private  ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	
+
+    /**
+     * Constructor to initialize the SharedData object with the array and target sum.
+     * 
+     * @param array The ArrayList of integers to check for a subset sum
+     * @param b The target sum
+     */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
@@ -22,22 +34,33 @@ public class SharedData
 	{
 		this.winArray = winArray;
 	}
+	//Gets the array of integers to check for a subset sum.
 
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+    /**
+     * Gets the target sum to find in the array.
+     * 
+     * @return The target sum `b`
+     */
+
 	public int getB() 
 	{
 		return b;
 	}
-
+	//Gets the flag that indicates if a solution has been found
 	public boolean getFlag() 
 	{
 		return flag;
 	}
-
+    /**
+     * Sets the flag that indicates if a solution has been found.
+     * 
+     * @param flag True if a solution has been found, false otherwise
+     */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
--- tar2threads/src/ThreadCheckArray.java
+++ tar2threads/src/ThreadCheckArray.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
-
+/**
+ * @author enabfouad
+ * This class implements the Runnable interface to check if a subset of an array
+ * adds up to a target value `b`. It uses recursion and multiple threads to 
+ * perform the subset sum check.
+ */
+ 
 public class ThreadCheckArray implements Runnable 
 {
 	private boolean flag;
-	private boolean [] winArray;
-	SharedData sd;
+	private boolean [] winArray;// Array to track the selected elements for the subset
+	SharedData sd;// Shared data object that holds the array, target value `b`, and other shared state
 	ArrayList<Integer> array;
 	int b;
+	//Constructor for the ThreadCheckArray class.
 	
 	public ThreadCheckArray(SharedData sd) 
 	{
@@ -18,7 +25,15 @@ public class ThreadCheckArray implements Runnable
 		}		
 		winArray = new boolean[array.size()];
 	}
-	
+	 /**
+     * Recursive function to check for a subset sum that equals `b`.
+     * This function works by considering two possibilities at each step:
+     * - Including the current element in the subset
+     * - Not including the current element in the subset
+     * 
+     * @param n The index of the current element being considered
+     * @param b The remaining target sum we need to achieve
+     */
 	void rec(int n, int b)
 	{
 		synchronized (sd) 
@@ -26,8 +41,9 @@ public class ThreadCheckArray implements Runnable
 			if (sd.getFlag())
 				return;
 		}	
-		if (n == 1)
+		if (n == 1)// Base case: Only one element left to check
 		{
+			// If the target sum is 0 or equal to the current element, a solution is found
 			if(b == 0 || b == array.get(n-1))
 			{
 				flag = true;
@@ -40,7 +56,8 @@ public class ThreadCheckArray implements Runnable
 				winArray[n-1] = true;
 			return;
 		}
-		
+
+        // Recursive calls: One where we include the current element and one where we don't
 		rec(n-1, b - array.get(n-1));
 		if (flag)
 			winArray[n-1] = true;
@@ -51,7 +68,10 @@ public class ThreadCheckArray implements Runnable
 		}	
 		rec(n-1, b);
 	}
-
+	  /**
+     * The `run` method is the entry point for the thread.
+     * It starts the recursive checking process either by including the last element or not.
+     */
 	public void run() {
 		if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
@@ -59,6 +79,7 @@ public class ThreadCheckArray implements Runnable
 			else 
 				rec(array.size()-1, b);
 		if (array.size() == 1)
+			
 			if (b == array.get(0) && !flag)
 			{
 				winArray[0] = true;
